39a15e46ea579aad0ebd8c39876cbcbd1908ad40
Khaled Omar <khaledomar4797@gmail.com>
2020-12-09 13:43:45 -0500

    Added NoteCreate class and Note Create View

--- ElevenNote.MVC/Controllers/NoteController.cs
+++ ElevenNote.MVC/Controllers/NoteController.cs
@@ -16,5 +16,23 @@ namespace ElevenNote.MVC.Controllers
             var model = new NoteListItem[0];
             return View();
         }
+
+        // GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+        }
     }
 }
